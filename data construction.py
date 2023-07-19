be8f97b4084e7fd7001cbbbcf1e212262116a806
SalinaLam <SalinaLam>
2023-07-19 08:09:52 +0000

    test

--- data construction.py
+++ data construction.py
@@ -9,10 +9,6 @@ features, target = make_regression(n_samples=100,
                                   random_state=42)
 
 # print features and target
-print("Features:")
-print(features[:5])
-print("Target:")
-print(target[:5])
 
 
 # COMMAND ----------
