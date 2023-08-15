5ec9414a69e2de328453a9b29be71e1428944220
SalinaLam <SalinaLam>
2023-08-15 10:47:32 +0000

    Updated data construction

--- data construction.py
+++ data construction.py
@@ -34,7 +34,13 @@ from sklearn import preprocessing
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ### Categorical
+# MAGIC
+# MAGIC ## Create Dataset
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Categorical Features
 
 # COMMAND ----------
 
@@ -46,23 +52,23 @@ from sklearn import preprocessing
 
 cat_variables = {
   'Occupation' : {
-    'names' : ['Data Scientist','Teacher','Nurse','Firefighter'],
-    'values' : [5,4,3,2,1]
+    'names' : ['Data Scientist','Teacher','Student','Nurse','Firefighter','Salesman'],
+    'values' : [6,5,4,3,2,1]
     },
   'Family Type' : {
     'names' : ['Family','Couple','Singles','Seniors'],
     'values': [4,3,2,1]
   },
   'Family Interest' : {
-    'names' :  ['Gaming','Sporty','Outdoor'],
-    'values': [3,2,1]
+    'names' :  ['Gaming','Musical','Crafty','Sporty','Travelling','Outdoor'],
+    'values': [6,5,4,3,2,1]
   },
   'House Type' : {
     'names' : ['Metal','Glas','Concrete','Tiles','Wood'],
     'values': [5,4,3,2,1]
   },
   'Geo Type' : {
-    'names' :['city','Suburban','rural'],
+    'names' :['Urban','Suburban','Rural'],
     'values': [3,2,1]
   }
 }
@@ -80,9 +86,17 @@ cat_features = ['Occupation','Family Type','Family Interest','House Type']
 
 # COMMAND ----------
 
-numerical_features = ['Number Of Residents', 'Average Age', 'Distance To Nearest Tower',
-                      'Number Of Phones','Number Of Computers','Number Of Tvs','Customer Happiness',
-                     'Time Spend On YouTube', 'Time Spend On TikTok', 'Time Spend On Instagram','Time Spend On Spotify']
+numerical_features = ['Number Of Residents', 'Average Age', 'Distance To Nearest Tower [m]',
+                      'Number Of Phones','Number Of Computers','Number Of Tvs', 'Number Of Pets'
+                      ,'Customer Happiness',
+                      'Time Spend On YouTube [min]', 'Time Spend On TikTok [min]', 
+                      'Time Spend On Instagram [min]','Time Spend On Spotify [min]',
+                      'Size of home [m2]', 
+                    ]
+
+# COMMAND ----------
+
+
 # numerical_features = {
 #    'Number Of Residents':{
 #      'mean':1, 
@@ -144,7 +158,8 @@ numerical_features = ['Number Of Residents', 'Average Age', 'Distance To Nearest
 # COMMAND ----------
 
 # total_features = list(numerical_features.keys())+list(cat_variables.keys())
-total_features = ['Number Of Residents','Time Spend On YouTube','Occupation','Mobile Traffic']
+# total_features = ['Number Of Residents','Time Spend On YouTube','Occupation','Mobile Traffic']
+total_features = cat_features + numerical_features + ['Mobile Traffic']
 
 # COMMAND ----------
 
@@ -156,6 +171,168 @@ for i in range(0,len(total_features)):
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC
+# MAGIC ### Update correlations
+
+# COMMAND ----------
+
+correlations_dict = {'Family Type - Occupation' : 0.6 ,
+'Family Interest - Occupation' : 0 ,
+'Family Interest - Family Type' : 0.5 ,
+'House Type - Occupation' : 0.4 ,
+'House Type - Family Type' : 0.4 ,
+'House Type - Family Interest' : 0 ,
+'Number Of Residents - Occupation' : 0 ,
+'Number Of Residents - Family Type' : 0 ,
+'Number Of Residents - Family Interest' : 0 ,
+'Number Of Residents - House Type' : 0 ,
+'Average Age - Occupation' : 0 ,
+'Average Age - Family Type' : -0.7 ,
+'Average Age - Family Interest' : 0 ,
+'Average Age - House Type' : 0 ,
+'Average Age - Number Of Residents' : 0.3 ,
+'Distance To Nearest Tower [m] - Occupation' : 0 ,
+'Distance To Nearest Tower [m] - Family Type' : 0 ,
+'Distance To Nearest Tower [m] - Family Interest' : 0 ,
+'Distance To Nearest Tower [m] - House Type' : 0 ,
+'Distance To Nearest Tower [m] - Number Of Residents' : 0 ,
+'Distance To Nearest Tower [m] - Average Age' : 0.4 ,
+'Number Of Phones - Occupation' : 0.5 ,
+'Number Of Phones - Family Type' : 0 ,
+'Number Of Phones - Family Interest' : 0 ,
+'Number Of Phones - House Type' : 0 ,
+'Number Of Phones - Number Of Residents' : 0.8 ,
+'Number Of Phones - Average Age' : 0.5 ,
+'Number Of Phones - Distance To Nearest Tower [m]' : 0 ,
+'Number Of Computers - Occupation' : 0.3 ,
+'Number Of Computers - Family Type' : 0.3 ,
+'Number Of Computers - Family Interest' : 0.3 ,
+'Number Of Computers - House Type' : 0 ,
+'Number Of Computers - Number Of Residents' : 0.8 ,
+'Number Of Computers - Average Age' : 0.5 ,
+'Number Of Computers - Distance To Nearest Tower [m]' : 0 ,
+'Number Of Computers - Number Of Phones' : 0.7 ,
+'Number Of Tvs - Occupation' : 0 ,
+'Number Of Tvs - Family Type' : 0 ,
+'Number Of Tvs - Family Interest' : 0.5 ,
+'Number Of Tvs - House Type' : 0 ,
+'Number Of Tvs - Number Of Residents' : 0.7 ,
+'Number Of Tvs - Average Age' : 0.5 ,
+'Number Of Tvs - Distance To Nearest Tower [m]' : 0 ,
+'Number Of Tvs - Number Of Phones' : 0.5 ,
+'Number Of Tvs - Number Of Computers' : 0.5 ,
+'Number Of Pets - Occupation' : 0 ,
+'Number Of Pets - Family Type' : 0 ,
+'Number Of Pets - Family Interest' : 0 ,
+'Number Of Pets - House Type' : 0 ,
+'Number Of Pets - Number Of Residents' : 0. ,
+'Number Of Pets - Average Age' : 0. ,
+'Number Of Pets - Distance To Nearest Tower [m]' : 0. ,
+'Number Of Pets - Number Of Phones' : 0. ,
+'Number Of Pets - Number Of Computers' : 0. ,
+'Number Of Pets - Number Of Tvs' : 0. ,
+'Customer Happiness - Occupation' : 0. ,
+'Customer Happiness - Family Type' : 0. ,
+'Customer Happiness - Family Interest' : 0. ,
+'Customer Happiness - House Type' : 0. ,
+'Customer Happiness - Number Of Residents' : 0. ,
+'Customer Happiness - Average Age' : 0. ,
+'Customer Happiness - Distance To Nearest Tower [m]' : -0.7 ,
+'Customer Happiness - Number Of Phones' : 0. ,
+'Customer Happiness - Number Of Computers' : 0. ,
+'Customer Happiness - Number Of Tvs' : 0. ,
+'Customer Happiness - Number Of Pets' : 0. ,
+'Time Spend On YouTube [min] - Occupation' : 0.5 ,
+'Time Spend On YouTube [min] - Family Type' : 0.5 ,
+'Time Spend On YouTube [min] - Family Interest' : 0.5 ,
+'Time Spend On YouTube [min] - House Type' : 0. ,
+'Time Spend On YouTube [min] - Number Of Residents' : 0.6 ,
+'Time Spend On YouTube [min] - Average Age' : -0.4 ,
+'Time Spend On YouTube [min] - Distance To Nearest Tower [m]' : 0. ,
+'Time Spend On YouTube [min] - Number Of Phones' : 0.5 ,
+'Time Spend On YouTube [min] - Number Of Computers' : 0.5 ,
+'Time Spend On YouTube [min] - Number Of Tvs' : 0. ,
+'Time Spend On YouTube [min] - Number Of Pets' : 0. ,
+'Time Spend On YouTube [min] - Customer Happiness' : 0. ,
+'Time Spend On TikTok [min] - Occupation' : 0. ,
+'Time Spend On TikTok [min] - Family Type' : 0.5 ,
+'Time Spend On TikTok [min] - Family Interest' : 0.5 ,
+'Time Spend On TikTok [min] - House Type' : 0. ,
+'Time Spend On TikTok [min] - Number Of Residents' : 0.5 ,
+'Time Spend On TikTok [min] - Average Age' : -0.7 ,
+'Time Spend On TikTok [min] - Distance To Nearest Tower [m]' : 0. ,
+'Time Spend On TikTok [min] - Number Of Phones' : 0.6 ,
+'Time Spend On TikTok [min] - Number Of Computers' : 0. ,
+'Time Spend On TikTok [min] - Number Of Tvs' : 0. ,
+'Time Spend On TikTok [min] - Number Of Pets' : 0. ,
+'Time Spend On TikTok [min] - Customer Happiness' : 0. ,
+'Time Spend On TikTok [min] - Time Spend On YouTube [min]' : 0.3 ,
+'Time Spend On Instagram [min] - Occupation' : 0. ,
+'Time Spend On Instagram [min] - Family Type' : 0.5 ,
+'Time Spend On Instagram [min] - Family Interest' : 0.3 ,
+'Time Spend On Instagram [min] - House Type' : 0. ,
+'Time Spend On Instagram [min] - Number Of Residents' : 0.3 ,
+'Time Spend On Instagram [min] - Average Age' : -0.3 ,
+'Time Spend On Instagram [min] - Distance To Nearest Tower [m]' : 0. ,
+'Time Spend On Instagram [min] - Number Of Phones' : 0.6 ,
+'Time Spend On Instagram [min] - Number Of Computers' : 0. ,
+'Time Spend On Instagram [min] - Number Of Tvs' : 0. ,
+'Time Spend On Instagram [min] - Number Of Pets' : 0. ,
+'Time Spend On Instagram [min] - Customer Happiness' : 0. ,
+'Time Spend On Instagram [min] - Time Spend On YouTube [min]' : 0.3 ,
+'Time Spend On Instagram [min] - Time Spend On TikTok [min]' : 0.5 ,
+'Time Spend On Spotify [min] - Occupation' : 0. ,
+'Time Spend On Spotify [min] - Family Type' : 0. ,
+'Time Spend On Spotify [min] - Family Interest' : 0. ,
+'Time Spend On Spotify [min] - House Type' : 0. ,
+'Time Spend On Spotify [min] - Number Of Residents' : 0.5 ,
+'Time Spend On Spotify [min] - Average Age' : -0.2 ,
+'Time Spend On Spotify [min] - Distance To Nearest Tower [m]' : 0. ,
+'Time Spend On Spotify [min] - Number Of Phones' : 0.5 ,
+'Time Spend On Spotify [min] - Number Of Computers' : 0.5 ,
+'Time Spend On Spotify [min] - Number Of Tvs' : 0. ,
+'Time Spend On Spotify [min] - Number Of Pets' : 0. ,
+'Time Spend On Spotify [min] - Customer Happiness' : 0. ,
+'Time Spend On Spotify [min] - Time Spend On YouTube [min]' : 0.5 ,
+'Time Spend On Spotify [min] - Time Spend On TikTok [min]' : 0.5 ,
+'Time Spend On Spotify [min] - Time Spend On Instagram [min]' : 0.5 ,
+'Size of home [m2] - Occupation' : 0.6 ,
+'Size of home [m2] - Family Type' : 0.6 ,
+'Size of home [m2] - Family Interest' : 0. ,
+'Size of home [m2] - House Type' : 0. ,
+'Size of home [m2] - Number Of Residents' : 0.7 ,
+'Size of home [m2] - Average Age' : 0.3 ,
+'Size of home [m2] - Distance To Nearest Tower [m]' : 0.6 ,
+'Size of home [m2] - Number Of Phones' : 0.3 ,
+'Size of home [m2] - Number Of Computers' : 0.3 ,
+'Size of home [m2] - Number Of Tvs' : 0.3 ,
+'Size of home [m2] - Number Of Pets' : 0.6 ,
+'Size of home [m2] - Customer Happiness' : 0. ,
+'Size of home [m2] - Time Spend On YouTube [min]' : 0. ,
+'Size of home [m2] - Time Spend On TikTok [min]' : 0. ,
+'Size of home [m2] - Time Spend On Instagram [min]' : 0. ,
+'Size of home [m2] - Time Spend On Spotify [min]' : 0. ,
+'Mobile Traffic - Occupation' : 0.3 ,
+'Mobile Traffic - Family Type' : 0.3 ,
+'Mobile Traffic - Family Interest' : 0.5 ,
+'Mobile Traffic - House Type' : 0.3 ,
+'Mobile Traffic - Number Of Residents' : 0.6 ,
+'Mobile Traffic - Average Age' : -0.4 ,
+'Mobile Traffic - Distance To Nearest Tower [m]' : -0.3 ,
+'Mobile Traffic - Number Of Phones' : 0.6 ,
+'Mobile Traffic - Number Of Computers' : 0.3 ,
+'Mobile Traffic - Number Of Tvs' : 0. ,
+'Mobile Traffic - Number Of Pets' : 0. ,
+'Mobile Traffic - Customer Happiness' : 0.6 ,
+'Mobile Traffic - Time Spend On YouTube [min]' : 0.6 ,
+'Mobile Traffic - Time Spend On TikTok [min]' : 0.3 ,
+'Mobile Traffic - Time Spend On Instagram [min]' : 0.3 ,
+'Mobile Traffic - Time Spend On Spotify [min]' : 0.1 ,
+'Mobile Traffic - Size of home [m2]' : 0.3 }
+
+# COMMAND ----------
+
 def random_correlation(max_random=0.05):
   return np.random.random()*max_random-max_random/2
 
@@ -164,14 +341,7 @@ cov_matrix = np.eye(len(total_features))
 
 variables = {x:i for i,x in enumerate(total_features)}
 
-correlations = {
-'Time Spend On YouTube - Number Of Residents' : 1,
-'Occupation - Number Of Residents' : 1 ,
-'Occupation - Time Spend On YouTube' : 0.0 ,
-'Mobile Traffic - Number Of Residents' : 1 ,
-'Mobile Traffic - Time Spend On YouTube' : 0 ,
-'Mobile Traffic - Occupation' : 0 
-}
+correlations = correlations_dict
 
 for corre in correlations:
   number = correlations[corre]
@@ -210,25 +380,71 @@ df.columns = total_features
 
 # COMMAND ----------
 
-#Transform numeric values
-df['Number Of Residents'] = np.floor(np.exp(df['Number Of Residents']*2)).astype(int)
-df['Time Spend On YouTube'] = np.exp(df['Time Spend On YouTube']*5.5)
-df['Mobile Traffic'] = df['Mobile Traffic']*20
+# MAGIC %md
+# MAGIC
+# MAGIC ### Transform numeric values
+# MAGIC
 
+# COMMAND ----------
+
+df['Number Of Residents'] = np.floor(np.exp(df['Number Of Residents']*2)).astype(int)
+df['Average Age'] = (df['Average Age']*60 + 20).astype(int)
+df['Distance To Nearest Tower [m]'] = df['Distance To Nearest Tower [m]'] * 2400
+df['Number Of Phones'] =(np.floor(df['Number Of Phones'] * 6)).astype(int)
+df['Number Of Computers'] = (np.floor(df['Number Of Computers'] * 3.3)).astype(int)
+df['Number Of Tvs'] = (np.floor(df['Number Of Tvs'] * 3.4)).astype(int)
+df['Number Of Pets'] = (np.floor(df['Number Of Pets'] * 2.5)).astype(int)
+df['Time Spend On YouTube [min]'] = np.exp(df['Time Spend On YouTube [min]']*4)
+df['Time Spend On TikTok [min]'] = np.exp(df['Time Spend On TikTok [min]']*4.5)
+df['Time Spend On Instagram [min]'] = np.exp(df['Time Spend On Instagram [min]']*5)
+df['Time Spend On Spotify [min]'] = np.exp(df['Time Spend On Spotify [min]']*6)
+df['Mobile Traffic'] = df['Mobile Traffic']*20
+df['Customer Happiness'] = pd.cut(df['Customer Happiness'] ,[0,0.2,0.21,0.22,0.3,0.35,0.4,0.5,0.65,0.7,1], labels = [1,2,3,4,5,6,7,8,9,10])
+
+
+# COMMAND ----------
+
+df['Customer Happiness'] = df['Customer Happiness'].fillna(8)
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+#export
+df_raw = df.copy()
 df.to_csv('transformed_data_raw.csv')
 
 
 # COMMAND ----------
 
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Transform data for visualisation
+
+# COMMAND ----------
+
 # Transform Categorical features from numeric to discrete
-bins= df['Occupation'].quantile(np.linspace(0,1,len(cat_variables['Occupation']['values'])))
-df['Occupation'] = pd.cut(df['Occupation'],bins, labels=cat_variables['Occupation']['names'])
+for i in cat_features:
+  bins= df[i].quantile(np.linspace(0,1,len(cat_variables[i]['values']) + 1))
+  df[i] = pd.cut(df[i],bins, labels=cat_variables[i]['names'])
+
 df.to_csv('transformed_data_for_viewing.csv')
 
 
 # COMMAND ----------
 
-df.hist()
+df.head()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Test Modelling
 
 # COMMAND ----------
 
@@ -237,13 +453,17 @@ from sklearn.model_selection import train_test_split
 
 # COMMAND ----------
 
-X = df.values[:,0:(I-1)]
-Y = df.values[:,(I-1):]
+X = df_raw.values[:,0:(I-1)]
+Y = df_raw.values[:,(I-1):]
 
 X_train, X_test, y_train, y_test = train_test_split(X,Y ,
                                    random_state=104, 
                                    test_size=0.25, 
                                    shuffle=True)
+
+# COMMAND ----------
+
+
 
 # COMMAND ----------
 
@@ -266,6 +486,13 @@ plot(np.linspace(0,15,10),np.linspace(0,15,10))
 # Create plots
 # Create models
 # Widgets for testing
+# 1 page for linear regression manual weights
+# 1 page for linear regression
+# 
 # Prep output
 # Live submits to track performence LEADERBOARD 
 # Notebook of "How to "
+
+# COMMAND ----------
+
+
