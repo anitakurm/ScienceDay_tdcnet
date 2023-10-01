5f44f3b218e245caaa85949a353ac2e593b5ed23
anitakurm <anitakurm@gmail.com>
2023-10-02 00:59:10 +0200

    train_test split added

--- workshop_03/Neural_net_scripts.py
+++ workshop_03/Neural_net_scripts.py
@@ -46,37 +46,43 @@ def set_up_layer_sliders():
   # set up slider layout
   layout = wd.Layout(width='auto', height='40px') #set width and height
   
-  layer_list =  ['Layer 1','Layer 2','Layer 3']
+  layer_list = ['Layer 1','Layer 2','Layer 3', 'Layer 4']
 
   sliders = { 
       i : wd.IntSlider(
         min=slider_min,
         max=slider_max,
         step=1,
-        description=i + ': ',
+        description='nodes: ',
         value=slider_value,
         #layout=wd.Layout(width='60%'),
         #style={'description_width': '100%'}
       ) for i in layer_list
     }
+  slider_split= wd.IntSlider( min=1,
+                            max=99,
+                            step=1,
+                            description=':',
+                            value=50,
+                          )
+  
+  sliders['% of data for train'] = slider_split
   return sliders
 
 
 def initiate_and_run_ann(X, y, hidden_layer_sizes=(2,1,3), train_size=0.75, max_iter=500):
-  scores = []
-  train_scores = []
-  for seed in [1, 42, 123]:
-      X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1-train_size, random_state=seed)
-      
-      model = MLPRegressor(hidden_layer_sizes = hidden_layer_sizes, 
-                          activation ='relu', alpha=0.0001,
-                          solver='adam', learning_rate='constant',
-                          max_iter= 800, n_iter_no_change =50, random_state=seed
-              )
-      model.fit(X_train, y_train)
-      scores.append(model.score(X_test, y_test))
-      train_scores.append(model.score(X_train, y_train))
-      #print("Seed: {}, Score: {}".format(seed, mlr.score(X_test, y_test)))
+
+  seed = 1
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1-train_size, random_state=seed)
+  
+  model = MLPRegressor(hidden_layer_sizes = hidden_layer_sizes, 
+                      activation ='relu', alpha=0.0001,
+                      solver='adam', learning_rate='constant',
+                      max_iter= 800, n_iter_no_change =50, random_state=seed
+          )
+  model.fit(X_train, y_train)
+  #score = model.score(X_test, y_test))
+  #train_score = model.score(X_train, y_train))
   
   actual_train = y_train
   predicted_train = model.predict(X_train)
@@ -84,7 +90,7 @@ def initiate_and_run_ann(X, y, hidden_layer_sizes=(2,1,3), train_size=0.75, max_
   actual_test = y_test
   predicted_test = model.predict(X_test)
   #print(np.average(scores))
-  return scores, train_scores, actual_train, predicted_train, actual_test, predicted_test
+  return actual_train, predicted_train, actual_test, predicted_test
 
 # ================= RESULT WIDGET ==================
 def result_text(mae, mape):
@@ -117,12 +123,12 @@ def result_text(mae, mape):
     
     return s + s2 + s3
 
-def plot_pred(ax, y_predicted, y_actual):
+def plot_pred(ax, y_predicted, y_actual, title):
     ax.clear()
     ax.scatter(y_predicted,y_actual, alpha=0.6)
     limits = [min(min(y), min(y)), max(max(y), max(y))]
     ax.plot(limits, limits, 'red', linestyle="dashed", linewidth=0.8)
-    ax.set_title("Mobile Traffic - ML Model")
+    ax.set_title(title)
     ax.set_ylabel("Actual")
     ax.set_xlabel("Predicted")
     return ax
@@ -140,10 +146,14 @@ fig2, ax2 = plt.subplots(layout='constrained')
 fig2.canvas.header_visible = False
 
 sliders = set_up_layer_sliders()
+
 slider_box = wd.HBox([
     wd.VBox([wd.HTML(v) for v in sliders]),
-    wd.VBox([v for v in sliders.values()])
+    wd.VBox([v for v in sliders.values()]),
 ])
+
+
+
 
 button_style = dict(
         button_color = "royalblue", 
@@ -165,27 +175,34 @@ df_raw = pd.read_csv('../data/transformed_data_raw.csv', index_col = 0)
 X = df_raw.drop(target_var, axis=1).values
 y = df_raw[target_var].values
 
+prediction_log = []
+prediction_log_df = pd.DataFrame(columns = [
+    "Time",
+    "Mean Absolute Error", 
+    "Mean Percentage Error",
+    "Architecture"
+])
+
 def click_button(b):
     # do prediction, get train and test results
     ann_architecture = []
-    layers= ['Layer 1','Layer 2','Layer 3']
     for var_name, slider in sliders.items(): 
-      print(f"{var_name}: {slider.value}")
-      layer_size = slider.value
-      ann_architecture.append(layer_size)
+      if 'Layer' in var_name:
+         layer_size = slider.value
+         ann_architecture.append(layer_size)
+      else:
+          train_size = slider.value/100
     
     ann_architecture_tup = tuple(ann_architecture)
     print(f'Training your neural net {ann_architecture_tup}')
-    scores, train_scores, actual_train, predicted_train, actual_test, predicted_test = initiate_and_run_ann(X, y, hidden_layer_sizes=ann_architecture_tup)
+    actual_train, predicted_train, actual_test, predicted_test = initiate_and_run_ann(X, y, train_size=train_size, hidden_layer_sizes=ann_architecture_tup)
 
     print('Ready!')
-    print(f'Your train scores: {train_scores}')
-    print(f'Your test scores: {scores}')
-    plot_pred(ax1, y_predicted = predicted_test, y_actual=actual_test)
+    plot_pred(ax1, y_predicted = predicted_test, y_actual=actual_test, title='Mobile Traffic - Neural Net (Test)')
     fig1.canvas.draw()
     fig1.canvas.flush_events()
     
-    plot_pred(ax2, y_predicted = predicted_train, y_actual=actual_train)
+    plot_pred(ax2, y_predicted = predicted_train, y_actual=actual_train, title='Mobile Traffic - Neural Net (Train)')
     fig2.canvas.draw()
     fig2.canvas.flush_events()
     mape = np.mean(np.abs((actual_test - predicted_test)/(np.maximum(actual_test, 0.1))))*100
@@ -220,104 +237,3 @@ main = wd.VBox([
     ])
 ])
     
-
-
-
-
-
-
-
-#############################################################################################3
-def run_get_slider_values():
-  df_raw = fetch_data()
-  target_var = "Mobile_Traffic"
-  
-  X = df_raw.drop(target_var, axis=1).values
-  y = df_raw[target_var].values
-  ann_architecture = []
-  layer_list =  ['Layer 1','Layer 2','Layer 3']
-  for i in layer_list: 
-    layer_size = globals()[f'slider_{i}'].value
-    ann_architecture.append(layer_size)
-  
-  ann_architecture_tup = tuple(ann_architecture)
-  score = initiate_and_run_ann(X, y, hidden_layer_sizes=ann_architecture_tup)
-  print(f'Yay!{score}')
-
-
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import ipywidgets as wd
-from scipy import stats
-import statsmodels.formula.api as smf
-from datetime import datetime as dtime
-print("Imports done")
-# Read Data
-df_raw = pd.read_csv('../data/transformed_data_raw.csv', index_col = 0)
-df_view = pd.read_csv('../data/transformed_data_for_viewing.csv', index_col = 0)
-
-# Hide last 200 observations for testing 
-#df_unseen = df_raw[-200:]
-#df = df_raw[:-200]
-
-#variables = list(df.columns)
-#target_var = "Mobile Traffic"
-#variables.remove(target_var)
-
-# standardizing dataframe so coefficients are -1 and 1
-df_z = df.select_dtypes(include=[np.number]).dropna().apply(stats.zscore)
-
-# Store mean and std to transform back 
-mean_std={}
-for var in df.columns:
-    mean_std[var]=(df[var].mean(), df[var].std())
-
-def reverse_zscore(pandas_series, mean, std):
-    '''Mean and standard deviation should be of original variable before standardization'''
-    yis=pandas_series*std+mean
-    return yis
-
-original_mean, original_std = mean_std[target_var]
-original_var_series = reverse_zscore(df_z[target_var], original_mean, original_std)
-
-Y_actual = df[target_var]
-print("Prepared data")
-
-prediction_log = []
-prediction_log_df = pd.DataFrame(columns = [
-    "Time",
-    "Mean Absolute Error", 
-    "Mean Percentage Error",
-    "Architecture"
-])
-
-
-
-def read_sliders():
-    weights = {var_name : slider.value/10 for var_name, slider in sliders.items()}
-    return weights
-
-  
-
-def run_ann_1():
-  ann_architecture = []
-  layer_list =  ['Layer 1','Layer 2','Layer 3', 'Layer 4', 'Layer 5']
-  for i in layer_list: 
-    layer_size = globals()[f'slider_{i}'].value
-    ann_architecture.append(layer_size)
-  
-  ann_architecture_tup = tuple(ann_architecture)
-  score = initiate_and_run_ann(hidden_layer_sizes=ann_architecture_tup)
-  print(f'Yay!{score}')
-
-def plot_manual_pred(ax, Y_pred_trans):
-    ax.clear()
-    ax.scatter(Y_pred_trans,Y_actual)
-    ax.plot([0, max(max(Y_actual), max(Y_pred_trans))], [0,max(max(Y_actual), max(Y_pred_trans))], 'red')
-    ax.set_title("Mobile Traffic - Manual Model")
-    ax.set_ylabel("Actual")
-    ax.set_xlabel("Predicted")
-    return ax
-
-
